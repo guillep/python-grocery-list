30ea95e50d57b99fd6aec5242d65d8eaa5eedcce
Guillermo <guillermopolito@gmail.com>
2017-11-13 12:31:35 +0100

    Remove unnecessary dependency to itertools

--- setup.py
+++ setup.py
@@ -67,6 +67,5 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['itertools','yaml'],
-
+    install_requires=['yaml'],
 )
